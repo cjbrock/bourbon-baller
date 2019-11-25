08eaebd24eb88e7dcdb6dc7037dcb465ba34edb7
cjbrock <corinna@flatironschool.com>
2019-11-25 11:35:12 -0500

    add index, new, create, and params

--- app/controllers/bourbons_controller.rb
+++ app/controllers/bourbons_controller.rb
@@ -1,2 +1,23 @@
 class BourbonsController < ApplicationController
+
+    def index
+        @bourbons = Bourbon.all
+    end
+
+    def new
+        @bourbon = Bourbon.new
+    end
+
+    def create
+        @bourbon = Bourbon.new(bourbon_params)
+        if @bourbon.save
+            # do something
+        else
+            # do something else
+        end
+    end
+
+    private
+
+    params.require(:bourbon).permit(:name, :type, :description, :distillery_id)
 end
